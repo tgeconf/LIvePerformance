2c8809ed08e4280eac6ec1aff6baea51f51b1284
tge <tgeconf@gmail.com>
2020-11-29 00:20:20 +0800

    Update bubble.js

--- js/bubble.js
+++ js/bubble.js
@@ -2,6 +2,7 @@ class Bubble {
     static sceneRangeX = 2000;
     static sceneRangeY = 2000;
     static sceneRangeZ = 5000;
+    static scaleStep = 0.2;
     static bubbles = [];
 
     static updateAllBubbles() {
@@ -27,24 +28,23 @@ class Bubble {
 
     init() {
         const that = this;
-        new Promise((resolve, reject) => {
-            that.bubbleDiv = document.createElement('div');
-            that.bubbleDiv.className = 'bubble';
-            that.bubbleDiv.style.width = (that.r * 2) + 'px';
-            that.bubbleDiv.style.height = (that.r * 2) + 'px';
-            that.bubbleDiv.style.background = 'rgb(' + that.color.r + ',' + that.color.g + ',' + that.color.b + ')';
-            that.bubbleDiv.style.boxShadow = '0px 0px 12px rgba(' + that.color.r + ',' + that.color.g + ',' + that.color.b + ', 0.5)';
-            that.bubbleDiv.style.border = '1px solid rgba(' + that.color.r + ',' + that.color.g + ',' + that.color.b + ', 0.25)';
-            // that.bubbleDiv.style.opacity = 0;
-            that.bubbleObj = new THREE.CSS3DObject(that.bubbleDiv);
-            that.bubbleObj.position.x = that.x;
-            that.bubbleObj.position.y = that.y;
-            that.bubbleObj.position.z = that.z;
-            Bubble.bubbles.push(that);
-            that.scene.add(that.bubbleObj);
-        }).then(() => {
-            that.bubbleDiv.style.opacity = that.opacity;
-        })
+        that.bubbleDiv = document.createElement('div');
+        that.bubbleDiv.className = 'bubble';
+        that.bubbleDiv.style.width = (that.r * 2) + 'px';
+        that.bubbleDiv.style.height = (that.r * 2) + 'px';
+        that.bubbleDiv.style.background = 'rgb(' + that.color.r + ',' + that.color.g + ',' + that.color.b + ')';
+        that.bubbleDiv.style.boxShadow = '0px 0px 12px rgba(' + that.color.r + ',' + that.color.g + ',' + that.color.b + ', 0.5)';
+        that.bubbleDiv.style.border = '1px solid rgba(' + that.color.r + ',' + that.color.g + ',' + that.color.b + ', 0.25)';
+        // that.bubbleDiv.style.opacity = 0;
+        that.bubbleObj = new THREE.CSS3DObject(that.bubbleDiv);
+        that.bubbleObj.position.x = that.x;
+        that.bubbleObj.position.y = that.y;
+        that.bubbleObj.position.z = that.z;
+        that.bubbleObj.scale.x = 0;
+        that.bubbleObj.scale.y = 0;
+        Bubble.bubbles.push(that);
+        that.scene.add(that.bubbleObj);
+        that.bubbleDiv.style.opacity = that.opacity;
     }
 
     update() {
@@ -60,5 +60,10 @@ class Bubble {
         this.bubbleObj.position.x += this.xSpeed;
         this.bubbleObj.position.y += this.ySpeed;
         this.bubbleObj.position.z += this.zSpeed;
+
+        if(this.bubbleObj.scale.x < 1){
+            this.bubbleObj.scale.x += Bubble.scaleStep;
+            this.bubbleObj.scale.y += Bubble.scaleStep;
+        }
     }
 }
