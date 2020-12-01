0ce524df0b281c0ebe9f269ae2440dcf9a4c39eb
tge <tgeconf@gmail.com>
2020-12-01 17:33:06 +0800

    Create spotLight.js

---
+++ js/spotLight.js
@@ -0,0 +1,11 @@
+class SpotLight {
+    constructor() {
+        this.startX;
+        this.startY;
+        this.startR;
+        this.endX;
+        this.endY;
+        this.endR;
+        this.canvas;
+    }
+}
